322b26e10bc56b68898d9305013e8d6990afc697
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2021-04-14 18:52:53 -0300

    commit projeto

---
+++ app/src/main/java/com/dantasbarreto/agendabootcamp/MainActivity.kt
@@ -0,0 +1,28 @@
+ package com.dantasbarreto.agendabootcamp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        btnSetEvent.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                    .setData(CONTENT_URI)
+                    .putExtra(TITLE, "Bootcamp everis")
+                    .putExtra(EVENT_LOCATION, "on line")
+                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000))
+
+            startActivity(intent)
+        }
+    }
+}
